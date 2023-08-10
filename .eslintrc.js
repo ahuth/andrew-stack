d4eeab7c45ce785e1354f5b066cf9616fbfe3492
Andrew Huth <git@huth.me>
2023-08-09 21:04:10 -0400

    Run prettier as part of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
   root: true,
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
   // We're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to
   // set the jest version explicitly.
@@ -15,4 +9,14 @@ module.exports = {
       version: 28,
     },
   },
+  plugins: [
+    'prettier',
+  ],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ['build/'],
 };
