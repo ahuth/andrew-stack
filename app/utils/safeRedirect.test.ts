fcb542f6237af4f7dc6be718471910d0453cc7da
Andrew Huth <git@huth.me>
2023-08-13 17:27:41 -0400

    Add tests for safeRedirect

---
+++ app/utils/safeRedirect.test.ts
@@ -0,0 +1,26 @@
+import {safeRedirect} from './safeRedirect';
+
+it('returns the default redirect when no destination is provided', () => {
+  expect(safeRedirect(null)).toEqual('/');
+});
+
+it('allows customizing the default redirect', () => {
+  expect(safeRedirect(null, '/cats')).toEqual('/cats');
+});
+
+it('returns the default redirect when a non-string destination is provided', () => {
+  // @ts-expect-error Bad type intentionally passed here.
+  expect(safeRedirect(5)).toEqual('/');
+});
+
+it('returns the default redirect if the destination starts with //', () => {
+  expect(safeRedirect('//somewhere')).toEqual('/');
+});
+
+it('returns the default redirect if the destination does not start with a slash', () => {
+  expect(safeRedirect('./foo/bar')).toEqual('/');
+});
+
+it('returns the provided value if it is a string that starts with /', () => {
+  expect(safeRedirect('/coffee')).toEqual('/coffee');
+});
