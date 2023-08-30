e23c546172c3891d088a3c7ce5fdf3db4202ff12
Andrew Huth <git@huth.me>
2023-08-29 21:56:50 -0400

    Silence a React log about its dev tools during tests

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -2,3 +2,7 @@ import {installGlobals} from '@remix-run/node';
 import '@testing-library/jest-dom';
 
 installGlobals();
+
+// Silence a React log about its dev tools during tests.
+// @ts-expect-error
+globalThis.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {isDisabled: true};
