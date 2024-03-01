6d82558300871cd6c91219e2a8fda26e8de9fac9
Andrew Huth <git@huth.me>
2024-03-01 14:50:09 -0500

    Remove unnecessary describe from tests

--- app/models/note.schema.test.ts
+++ app/models/note.schema.test.ts
@@ -1,52 +1,50 @@
-import {describe, it, expect} from 'vitest';
+import {it, expect} from 'vitest';
 import {noteSchema} from './note.schema';
 
-describe('noteSchema', () => {
-  it('parses', () => {
-    const result = noteSchema.parse({
-      title: 'title',
-      body: 'body',
-    });
-
-    expect(result).toEqual({
-      title: 'title',
-      body: 'body',
-    });
+it('parses', () => {
+  const result = noteSchema.parse({
+    title: 'title',
+    body: 'body',
   });
 
-  it('requires title', () => {
-    const result = noteSchema.safeParse({
-      title: undefined,
-      body: 'body',
-    });
+  expect(result).toEqual({
+    title: 'title',
+    body: 'body',
+  });
+});
 
-    expect(result.success).toBe(false);
+it('requires title', () => {
+  const result = noteSchema.safeParse({
+    title: undefined,
+    body: 'body',
   });
 
-  it('does not allow an empty string as the title', () => {
-    const result = noteSchema.safeParse({
-      title: '',
-      body: 'body',
-    });
+  expect(result.success).toBe(false);
+});
 
-    expect(result.success).toBe(false);
+it('does not allow an empty string as the title', () => {
+  const result = noteSchema.safeParse({
+    title: '',
+    body: 'body',
   });
 
-  it('requires body', () => {
-    const result = noteSchema.safeParse({
-      title: 'title',
-      body: undefined,
-    });
+  expect(result.success).toBe(false);
+});
 
-    expect(result.success).toBe(false);
+it('requires body', () => {
+  const result = noteSchema.safeParse({
+    title: 'title',
+    body: undefined,
   });
 
-  it('does not allow an empty string as the body', () => {
-    const result = noteSchema.safeParse({
-      title: 'title',
-      body: '',
-    });
+  expect(result.success).toBe(false);
+});
 
-    expect(result.success).toBe(false);
+it('does not allow an empty string as the body', () => {
+  const result = noteSchema.safeParse({
+    title: 'title',
+    body: '',
   });
+
+  expect(result.success).toBe(false);
 });
--- app/models/user.schema.test.ts
+++ app/models/user.schema.test.ts
@@ -1,55 +1,53 @@
-import {describe, it, expect} from 'vitest';
+import {it, expect} from 'vitest';
 import {loginSchema} from './user.schema';
 
-describe('loginSchema', () => {
-  it('parses', () => {
-    const result = loginSchema.parse({
-      email: 'jane@example.com',
-      password: 'coffee is life',
-    });
-
-    expect(result).toEqual({
-      email: 'jane@example.com',
-      password: 'coffee is life',
-    });
+it('parses', () => {
+  const result = loginSchema.parse({
+    email: 'jane@example.com',
+    password: 'coffee is life',
   });
 
-  it('requires an email address', () => {
-    const result = loginSchema.safeParse({
-      email: undefined,
-      password: 'coffee is life',
-    });
+  expect(result).toEqual({
+    email: 'jane@example.com',
+    password: 'coffee is life',
+  });
+});
 
-    if (result.success) {
-      throw new Error('Expected result.success to be false');
-    }
-
-    expect(result.error.issues[0].message).toEqual('Email is required');
+it('requires an email address', () => {
+  const result = loginSchema.safeParse({
+    email: undefined,
+    password: 'coffee is life',
   });
 
-  it('requires a valid email address', () => {
-    const result = loginSchema.safeParse({
-      email: 'hi',
-      password: 'coffee is life',
-    });
+  if (result.success) {
+    throw new Error('Expected result.success to be false');
+  }
 
-    if (result.success) {
-      throw new Error('Expected result.success to be false');
-    }
+  expect(result.error.issues[0].message).toEqual('Email is required');
+});
 
-    expect(result.error.issues[0].message).toEqual('Email is invalid');
+it('requires a valid email address', () => {
+  const result = loginSchema.safeParse({
+    email: 'hi',
+    password: 'coffee is life',
   });
 
-  it('requires a password', () => {
-    const result = loginSchema.safeParse({
-      email: 'jane@example.com',
-      password: undefined,
-    });
+  if (result.success) {
+    throw new Error('Expected result.success to be false');
+  }
 
-    if (result.success) {
-      throw new Error('Expected result.success to be false');
-    }
+  expect(result.error.issues[0].message).toEqual('Email is invalid');
+});
 
-    expect(result.error.issues[0].message).toEqual('Password is required');
+it('requires a password', () => {
+  const result = loginSchema.safeParse({
+    email: 'jane@example.com',
+    password: undefined,
   });
+
+  if (result.success) {
+    throw new Error('Expected result.success to be false');
+  }
+
+  expect(result.error.issues[0].message).toEqual('Password is required');
 });
