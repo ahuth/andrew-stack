b02bee8246aaa0c1a705170cc7ffe9e67feae5c1
Andrew Huth <git@huth.me>
2024-06-10 10:41:57 -0400

    I totally edited this comment!

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -2,7 +2,7 @@ import {Link as RemixLink, type LinkProps as Props} from '@remix-run/react';
 import clsx from 'clsx';
 
 /**
- * Remix link styled to look like a link (blue, underline, etc).
+ * A re-usable link component that does all the link stuff.
  */
 export default function Link({className, ...props}: Props) {
   return (
