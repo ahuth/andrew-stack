694d4c6e029fd4b8264ea3317c46ec6dfea458ab
Andrew Huth <git@huth.me>
2024-09-07 08:21:37 -0400

    Use Vite for Storybook, instead of webpack

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,73 +1,17 @@
-import path from 'node:path';
-import type {StorybookConfig} from '@storybook/react-webpack5';
+import type {StorybookConfig} from '@storybook/react-vite';
 
 export default {
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-essentials',
-    '@storybook/addon-webpack5-compiler-swc',
-  ],
+  stories: ['../app'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.storybook.config.ts',
+      },
+    },
+  },
   core: {
     disableTelemetry: true,
   },
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
-  stories: ['../app'],
-
-  swc: () => ({
-    jsc: {
-      transform: {
-        react: {
-          runtime: 'automatic',
-        },
-      },
-    },
-  }),
-
-  webpackFinal(config) {
-    return {
-      ...config,
-
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve?.alias,
-          // Custom path aliases. Keep in sync with `paths` in tsconfig.json.
-          '~': path.resolve(__dirname, '../app'),
-          tests: path.resolve(__dirname, '../tests'),
-        },
-        fallback: {
-          ...config.resolve?.fallback,
-          // Suppress un-polyfilled module warning from @sinonjs/fake-timers.
-          timers: false,
-        },
-      },
-
-      module: {
-        ...config.module,
-        rules: [
-          ...(config.module?.rules || []),
-          // Replace @remix-run/node with an empty file. It should never be used by browser code.
-          {
-            test: require.resolve('@remix-run/node'),
-            use: 'null-loader',
-          },
-          // Replace `.server.ts` files with an empty file. Remix will never send these to the
-          // browser.
-          {
-            test: /\.server/,
-            use: 'null-loader',
-          },
-          // Integrate Tailwind. Remix handles this when running the app, but we need to configure it
-          // ourselves in Storybook.
-          {
-            test: /\.css$/i,
-            use: ['postcss-loader'],
-          },
-        ],
-      },
-    };
-  },
+  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials'],
 } satisfies StorybookConfig;
---
+++ vite.storybook.config.ts
@@ -0,0 +1,44 @@
+import {init, parse} from 'es-module-lexer';
+import {defineConfig, type Plugin} from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    nullLoader('.server'),
+    nullLoader('@remix-run/node'),
+  ],
+});
+
+/**
+ * Prevent Vite from including content in certain modules. Especially server-side code, which we
+ * don't want in Storybook builds.
+ */
+function nullLoader(pattern: string): Plugin {
+  return {
+    name: 'null-loader',
+    enforce: 'pre',
+    async transform(code, id) {
+      if (id.includes(pattern)) {
+        await init;
+        // Find exports
+        const [, exports] = parse(code);
+        // Create a module that is empty other than exporting the same names it had before.
+        return {
+          code:
+            'export default {};\n' +
+            exports
+              // Don't include the "default" export, which is already accounted for above.
+              .filter((exp) => exp.n !== 'default')
+              // Null value for each exported name.
+              .map((exp) => `export const ${exp.n} = null;`)
+              .join('\n'),
+          // Preserve existing sourcemaps.
+          map: null,
+          // Automagically create named exports for all properties of the default export.
+          syntheticNamedExports: true,
+        };
+      }
+    },
+  };
+}
