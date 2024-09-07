f33eb7e7dce885f59d556c1a1b1ca5c9d4c05cff
Andrew Huth <git@huth.me>
2024-09-06 22:31:14 -0400

    Remove unneeded test setup

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,7 +1,1 @@
-import {installGlobals} from '@remix-run/node';
 import '@testing-library/jest-dom/vitest';
-
-installGlobals();
-
-// @ts-expect-error Silence a React log about its dev tools during tests.
-globalThis.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {isDisabled: true};
